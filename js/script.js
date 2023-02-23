1c8a17e32f5b6c15af83b8caf4c1b28fae772dda
Ben Cruise <ben_cruise@hotmail.co.uk>
2023-02-23 21:54:15 +0000

    major updates to js and styling, also to add email.js

--- js/script.js
+++ js/script.js
@@ -36,11 +36,12 @@ $('.user-message').on('input', function(event) {userMessage = event.target.value
 
 $('.submit-button').on('click', function(event) {
   event.preventDefault();
+  alert('Email sent!');
   emailjs.send("service_sm9sfhi","template_o1do94o", {
     user_name: userName,
     user_email: userEmail,
     message: userMessage,
-    });
+    },'-29jWKGdLoYcM0kno');
 })
 
 
@@ -69,6 +70,7 @@ for (let a = 0; a < skills.length; a++) {
 for (let b = 0; b < work.length; b++) {
     let project = ``;
     let stack = ``;
+    let buttonHTML = ``;
 
     //create skills container
     for (let c = 0; c < work[b].techStack.length; c++) {
@@ -77,25 +79,35 @@ for (let b = 0; b < work.length; b++) {
                   </div>`;
     }
     
+    if (work[b].title == 'ReadMe Generator') {
+        buttonHTML = `<div class="buttons-div pt-4 col-lg-12 col-md-10">
+                        <a class="col-12" href=${work[b].gitHubRepoURL}>
+                          <button class="gitHub-repo-button p-2">GitHub Repo</button/>
+                        </a>
+                      </div>`
+    } else {
+        buttonHTML = `<div class="buttons-div pt-4 col-lg-12 col-md-10">
+                          <a class="col-6" href=${work[b].gitHubPagesURL}>
+                            <button class="case-study-button p-2">CASE STUDY</button/>
+                          </a>
+                          <a class="col-6" href=${work[b].gitHubRepoURL}>
+                            <button class="gitHub-repo-button p-2">GitHub Repo</button/>
+                          </a>
+                        </div>`
+    }
+
     project = `<div class="project-container p-5 row">
                       <span class="project-details-title">${work[b].title}</span>
-                      <div class="col-8">
+                      <div class="col-lg-8 col-mg-12 img-div">
                         <img src=${work[b].image} alt=${work[b].title}}></img>
                       </div>
-                      <div class="project-details col-4">                        
-                        <p>${work[b].description}</p>
-                        <div class="stack row">
+                      <div class="project-details col-lg-4 col-md-8 col-sm-10">                        
+                        <p class="col-12">${work[b].description}</p>
+                        <div class="stack row col-12">
                           ${stack}
                         </div>
                       </div>
-                      <div class="buttons-div pt-4 col-12">
-                        <a class="col-6" href=${work[b].gitHubPagesURL}>
-                          <button class="case-study-button p-2">CASE STUDY</button/>
-                        </a>
-                        <a class="col-6" href=${work[b].gitHubRepoURL}>
-                          <button class="gitHub-repo-button p-2">GitHub Repo</button/>
-                        </a>
-                      </div>
+                      ${buttonHTML}
                    </div>`
 
     $('.main-projects-container').append(project);
